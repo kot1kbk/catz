ed762861aadfae9e4b8ef3dd0b186f17c0fd69e2
kot1kbk <alinamalinka122@gmail.com>
2026-07-20 19:05:11 +0700

    Create help.zig

---
+++ src/help.zig
@@ -0,0 +1,17 @@
+pub const help_text =
+    \\catz version 0.1.0
+    \\Usage: catz [OPTION]... [FILE]...
+    \\
+    \\Concatenate FILE(s) to standard output.
+    \\
+    \\Options:
+    \\  -n, --number          number all output lines
+    \\  -b, --number-nonblank number non-empty output lines
+    \\  -E, --show-ends       display $ at end of each line
+    \\  -s, --squeeze-blank   suppress repeated empty lines
+    \\      --help            display this help and exit
+    \\
+    \\Examples:
+    \\  catz file.txt
+    \\  catz -n file1.txt file2.txt
+;
